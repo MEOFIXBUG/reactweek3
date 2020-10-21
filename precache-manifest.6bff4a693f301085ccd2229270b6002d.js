adb177d805460461d68b638942e99b5953a51331
SangDoan <doansang654@gmail.com>
2020-10-21 18:05:28 +0700

    Updates

---
+++ precache-manifest.6bff4a693f301085ccd2229270b6002d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7fdec024aa618b2ada77fccd3cebb7",
+    "url": "/reactweek3/index.html"
+  },
+  {
+    "revision": "dd187291c17e69d92773",
+    "url": "/reactweek3/static/css/main.11e981f0.chunk.css"
+  },
+  {
+    "revision": "a985c91d3afcf2305104",
+    "url": "/reactweek3/static/js/2.1b58fffb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/reactweek3/static/js/2.1b58fffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd187291c17e69d92773",
+    "url": "/reactweek3/static/js/main.b6565a8a.chunk.js"
+  },
+  {
+    "revision": "74f8d401f72a12efcf63",
+    "url": "/reactweek3/static/js/runtime-main.6aea2853.js"
+  }
+]);
